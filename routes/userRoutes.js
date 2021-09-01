411ba48525a7469884ea7a7b359bf162514f07c2
Zachary Taylor <zpanther8920@yahoo.com>
2021-09-01 19:59:30 -0400

    saved routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const { User, validateUser, userSchema } = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
 // const { models } = require("mongoose");
 
-router.post("/", async (req, res) => {
+//register
+router.post("/:", auth, async (req, res) => {
   try {
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -19,11 +21,61 @@ router.post("/", async (req, res) => {
     });
     await user.save();
     const token = user.generateAuthToken();
-    
+
     return res
       .header("x-auth-token", token)
       .header("access-control-expose-headers", "x-auth-token")
       .send({ _id: user._id, name: user.name, email: user.email });
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+router.put("/:userId/shoppingcart/:productId", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error);
+    const user = await User.findById(req.params.userId);
+    if (!user)
+      return res
+        .status(400)
+        .send(`The user with id "${req.params.userId}" does not exist.`);
+    const product = user.shoppingCart.id(req.params.productId);
+    if (!product)
+      return res
+        .status(400)
+        .send(
+          `The product with id "${req.params.productId}" does not in the users shopping cart.`
+        );
+    product.name = req.body.name;
+    product.description = req.body.description;
+    product.category = req.body.category;
+    product.price = req.body.price;
+    product.dateModified = Date.now();
+    await user.save();
+    return res.send(product);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+router.delete("/:userId/shoppingcart/:productId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user)
+      return res
+        .status(400)
+        .send(`The user with id "${req.params.userId}" does not exist.`);
+    let product = user.shoppingCart.id(req.params.productId);
+    if (!product)
+      return res
+        .status(400)
+        .send(
+          `The product with id "${req.params.productId}" does not in the users shopping cart.`
+        );
+    product = await product.remove();
+    await user.save();
+    return res.send(product);
   } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
   }
