398bab07faaf9e1dd10802849d9b7255a150b939
asiaromaria <designsbyarc@gmail.com>
2021-08-31 19:11:32 -0700

    routes for user sign in and authentication

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const  Joi  =  require('joi'); 
+const  bcrypt  =  require('bcrypt'); 
+const  express  =  require('express'); 
+const  {  User  }  =  require('../models/userModel');
+const  router  =  express.Router(); 
+const  config  =  require('config'); const  jwt  =  require('jsonwebtoken'); 
+const { isError } = require('joi');
+
+
+router.post('/', async (req, res) => {
+    try{
+        const { error } = validateLogin(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(400).send('Invalid email or password.');
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validPassword) return res.status(400).send('Invalid email or password.');
+        
+        const token = jwt.sign({ _id: user._id, name: user.name }, config.get('jwtSecret'));
+
+        return res.send(true);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+function  validateLogin(req)  { 
+    const  schema  =  Joi.object({ 
+        email:  Joi.string().min(5).max(255).required().email(), 
+        password:  Joi.string().min(5).max(1024).required(), 
+    }); 
+    return  schema.validate(req); 
+} 
+
+module.exports  =  router; 
