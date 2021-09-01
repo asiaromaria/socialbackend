900f07ee7ce39cc63fccf8791f359029bdcda950
Zachary Taylor <zpanther8920@yahoo.com>
2021-09-01 19:58:10 -0400

    creating the put and delete routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,30 +1,32 @@
-const { User, validateUser, userSchema } = require('../models/userModel');
-const express = require('express');
+const { User, validateUser, userSchema } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require("bcrypt")
 // const { models } = require("mongoose");
 
-
-router.post('/', async (req, res) => {
-try {
+router.post("/", async (req, res) => {
+  try {
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send('User already registered.');
+    if (user) return res.status(400).send("User already registered.");
     const salt = await bcrypt.genSalt(10);
     user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: await bcrypt.hash(req.body.password, salt),
+      name: req.body.name,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, salt),
     });
     await user.save();
-    return res.send({ _id: user._id, name: user.name, email: user.email });
-    } catch (ex) {
+    const token = user.generateAuthToken();
+    
+    return res
+      .header("x-auth-token", token)
+      .header("access-control-expose-headers", "x-auth-token")
+      .send({ _id: user._id, name: user.name, email: user.email });
+  } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+  }
 });
-
-
 
 module.exports = router;
