542469bba743fdbe888acc1657ae42a115aef475
calebbartley <calebbartley18@gmail.com>
2021-09-02 23:07:28 -0400

    auth and routing revised

--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 const Joi = require ('joi');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 
 
 const userSchema = new mongoose.Schema({
-    name:  { type: String, required: true, minlength: 5, maxlength: 50 },
+    name:  { type: String, required: true, minlength: 2, maxlength: 50 },
     email: { type: String, unique: true, required: true, minlength: 5, maxlength: 255 },
     password: { type: String, required: true, minlength: 5, maxlength: 1024 },
+    isAdmin: {type: Boolean, default: true },
 });
 
 
 userSchema.methods.generateAuthToken = function () {
-    return jwt.sign({ _id: this._id, name: this.name }, config.get('jwtSecret'));
-};   
+    return jwt.sign({ _id: this._id, name: this.name , isAdmin: this.isAdmin}, config.get('jwtSecret'));
+};
 
 
 const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
     const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
+        name: Joi.string().min(2).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(1024).required(),
     });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,30 @@
 const { User, validateUser, userSchema } = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const auth = require("../middleware/auth");
+const admin = require('../middleware/admin');
 const express = require("express");
 const router = express.Router();
 
 // const { models } = require("mongoose");
+// All users
+router.get('/', [auth,admin], async(req,res)=>{
+  try{
+    const users = await User.find();
+    return res.send(users);
+  } catch(ex){
+    return res.status(500).send(`Internal Server Error:${ex}`);
+  }
+})
+
+router.get('/userId', [auth,admin], async(req,res)=>{
+  try{
+    const user = await User.findById(req.params.userId);
+    if (!user) return res.status(400).send(`The user with id "${req.params.userId}" does not exist.`);
+  
+  } catch(ex){
+    return res.status(500).send(`Internal Server Error:${ex}`);
+  }
+})
 
 router.post('/', async (req, res) => {
   try {
@@ -19,7 +39,13 @@ router.post('/', async (req, res) => {
     password: await bcrypt.hash(req.body.password, salt),
   });
     await user.save();
-    return res.send({ _id: user._id, name: user.name, email: user.email });
+    const token = user.generateAuthToken();
+
+
+    return res
+      .header("x-auth-token", token)
+      .header("access-control-expose-headers", "x-auth-token")
+      .send({ _id: user._id, name: user.name, email: user.email });
   } catch (ex) {
   return res.status(500).send(`Internal Server Error: ${ex}`);
   }
@@ -51,7 +77,7 @@ router.post('/', async (req, res) => {
 
 
 //register
-router.post("/:userId/shoppingcart/:productId", auth, async (req, res) => {
+router.post("/", [auth, admin], async (req, res) => {
   try {
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -75,52 +101,56 @@ router.post("/:userId/shoppingcart/:productId", auth, async (req, res) => {
   }
 });
 
-router.put("/:userId/shoppingcart/:productId", auth, async (req, res) => {
+
+
+router.put("/:userId", [auth,admin], async (req, res) => {
   try {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error);
-    const user = await User.findById(req.params.userId);
+    const salt = await bcrypt.genSalt(10);
+    let user = await User.findByIdAndUpdate(req.params.userId,{
+      name : req.body.name,
+      email : req.body.email,
+      password : await bcrypt.hash(req.body.password, salt),},
+      {new: true}
+      );
     if (!user)
       return res
         .status(400)
         .send(`The user with id "${req.params.userId}" does not exist.`);
-    const product = user.shoppingCart.id(req.params.productId);
-    if (!product)
-      return res
-        .status(400)
-        .send(
-          `The product with id "${req.params.productId}" does not in the users shopping cart.`
-        );
-    product.name = req.body.name;
-    product.description = req.body.description;
-    product.category = req.body.category;
-    product.price = req.body.price;
-    product.dateModified = Date.now();
+    
+    
     await user.save();
-    return res.send(product);
+    const token = user.generateAuthToken();
+
+    return res
+      .header("x-auth-token", token)
+      .header("access-control-expose-headers", "x-auth-token")
+      .send({ _id: user._id, name: user.name, email: user.email });
+
   } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
   }
 });
 
 //
-router.delete("/:userId/shoppingcart/:productId", auth, async (req, res) => {
+router.delete("/:userId", auth, async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
     if (!user)
       return res
         .status(400)
         .send(`The user with id "${req.params.userId}" does not exist.`);
-    let product = user.shoppingCart.id(req.params.productId);
-    if (!product)
+    let users = user.id(req.params.userId);
+    if (!user)
       return res
         .status(400)
         .send(
-          `The product with id "${req.params.productId}" does not in the users shopping cart.`
+          `The user with id "${req.params.userId}" does not in the users shopping cart.`
         );
-    product = await product.remove();
+    user = await user.remove();
     await user.save();
-    return res.send(product);
+    return res.send(user);
   } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
   }
